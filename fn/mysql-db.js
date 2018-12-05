e7294388f2f966ab5552dcf3dddf282eb5f2d448
Phụng Minh <1512414@student.hcmus.edu.vn>
2018-12-05 14:39:11 +0700

    ok

--- fn/mysql-db.js
+++ fn/mysql-db.js
@@ -2,10 +2,11 @@ var mysql = require('mysql');
 
 var createConnection = () => {
     return mysql.createConnection({
-    	host: 'localhost',
-    	user: 'root',
-    	password: '',
-    	database: 'uber'
+    	host: 'ec2-107-21-125-209.compute-1.amazonaws.com',
+    	user: 'kiegajnyngpwrs',
+    	password: 'cdad120574e7165d6f972ed79f3233e136711a993dde0b5387bfc9385e6c6afb',
+    	database: 'd7khr1v5k6j3e0',
+        port:5432
     });
 }
 
---
+++ uber.sql
@@ -0,0 +1,10 @@
+INSERT INTO users (ID, Username, Name, Password, AccountType_ID, Status) VALUES
+(1, 'admin', 'Receiver', '21232f297a57a5a743894a0e4a801fc3', 1, 1),
+(2, 'admin2', 'Identifier', '21232f297a57a5a743894a0e4a801fc3', 2, 1),
+(3, 'admin3', 'Driver', '21232f297a57a5a743894a0e4a801fc3', 3, 1),
+(4, 'abc', 'abc', '900150983cd24fb0d6963f7d28e17f72', 1, 1),
+(5, 'abc', 'abc', '900150983cd24fb0d6963f7d28e17f72', 1, 1),
+(6, 'abc', 'abc', '900150983cd24fb0d6963f7d28e17f72', 1, 1),
+(7, 'abc', 'abc', '900150983cd24fb0d6963f7d28e17f72', 1, 1),
+(8, 'abc', 'abc', '900150983cd24fb0d6963f7d28e17f72', 1, 1),
+(9, 'admin4', 'Manager', '21232f297a57a5a743894a0e4a801fc3', 4, 1);
